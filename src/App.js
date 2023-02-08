0f79cdf811c62a6ee34d5e84e7958a830a818ee4
Fidelis <fideliofidel9@gmail.com>
2023-02-08 10:13:08 +0300

    Form created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from 'react'
 
 function App() {
+    
+    function createTodo(event) {
+        event.preventDefault();
+    }
     return (
-        <></>
+        <div>
+            <h1>Track Your Daily Tasks</h1>
+
+            <form onSubmit={createTodo}>
+                <input
+                    type='text'
+                    placeholder='Add New Task Here...'
+                />
+
+                <button type='submit'>Create Task</button>
+            </form>
+        </div>
     )
 }
 
