6052e20edfaf9c759a5f9f3fe62eb6af2b6f996a
Fidelis <fideliofidel9@gmail.com>
2023-02-08 10:55:08 +0300

    App functions and events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react'
 
+// Unique id for every task
+let globalID = 0;
+
 function App() {
+
+    // State to add task
+    const [task, setTask] = useState("");
+
+    // State to handle items
+    const [todos, setTodos] = useState([]);
     
+    // Function to handle form submit & add items to <li>
     function createTodo(event) {
         event.preventDefault();
+        
+        setTodos(prevTodo => {
+            setTask('')
+            return [...prevTodo, { todo: task, id: globalID++ }]
+        })
     }
+
+    // Function to delete Task
+    function deleteTaskItem(itemID) {
+        setTodos(prevTodo => prevTodo.filter(item => item.id !== itemID))
+    }
+
     return (
         <div>
             <h1>Track Your Daily Tasks</h1>
@@ -13,10 +34,23 @@ function App() {
                 <input
                     type='text'
                     placeholder='Add New Task Here...'
+                    value={task}
+                    onChange={event => {
+                        setTask(event.target.value)
+                    }}
                 />
 
                 <button type='submit'>Create Task</button>
             </form>
+
+            <ul>
+                {todos.map((item) => {
+                    return <div key={item.id}>
+                        <li>{item.todo}</li>
+                        <button onClick={() => deleteTaskItem(item.id)}>Delete</button>
+                    </div>
+                })}
+            </ul>
         </div>
     )
 }
