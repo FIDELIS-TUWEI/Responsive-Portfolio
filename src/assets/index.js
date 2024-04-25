00669793c80ab19e02e3860005623fa0849cfb62
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-25 13:12:24 +0300

    index file for dynamix imports created

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import Project1 from "./projects/project-1.jpg";
+import Project2 from "./projects/project-2.jpg";
+import Project3 from "./projects/project-3.jpg";
+import Project4 from "./projects/project-4.jpg";
+import About from "../assets/about.jpg";
+import Logo from "../assets/kevinRushLogo.png";
+import Profile from "../assets/kevinRushProfile.png";
+import KevinProfile from "../assets/kevinRushProfile.jpg";
+
+
+export {
+    Project1, Project2, Project3, Project4,
+    About, Logo, Profile, KevinProfile
+}
