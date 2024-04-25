5df086aaa56855532d682503d440ad931ea65581
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-25 13:50:09 +0300

    import changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,11 +4,11 @@ import Project3 from "./projects/project-3.jpg";
 import Project4 from "./projects/project-4.jpg";
 import About from "../assets/about.jpg";
 import Logo from "../assets/kevinRushLogo.png";
-import Profile from "../assets/kevinRushProfile.png";
+import ProfilePic from "../assets/kevinRushProfile.png";
 import KevinProfile from "../assets/kevinRushProfile.jpg";
 
 
 export {
     Project1, Project2, Project3, Project4,
-    About, Logo, Profile, KevinProfile
+    About, Logo, ProfilePic, KevinProfile
 }
