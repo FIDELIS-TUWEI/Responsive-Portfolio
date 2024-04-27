bf0269c7c936ecd28bc2bdbaf699afb31d70af00
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-27 11:07:18 +0300

    Assets renaming about image

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import Project1 from "./projects/project-1.jpg";
 import Project2 from "./projects/project-2.jpg";
 import Project3 from "./projects/project-3.jpg";
 import Project4 from "./projects/project-4.jpg";
-import About from "../assets/about.jpg";
+import AboutImg from "../assets/about.jpg";
 import Logo from "../assets/kevinRushLogo.png";
 import ProfilePic from "../assets/kevinRushProfile.png";
 import KevinProfile from "../assets/kevinRushProfile.jpg";
@@ -10,5 +10,5 @@ import KevinProfile from "../assets/kevinRushProfile.jpg";
 
 export {
     Project1, Project2, Project3, Project4,
-    About, Logo, ProfilePic, KevinProfile
+    AboutImg, Logo, ProfilePic, KevinProfile
 }
