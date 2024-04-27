1c55d122872af90a8beb875e052c5fc9f50f57e6
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-27 13:22:16 +0300

    Contact UI component dynamic import

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@ import About from "./About";
 import Technologies from "./Technologies";
 import Experience from "./Experience";
 import Projects from "./Projects";
+import Contact from "./Contact";
 
 export {
-    Navbar, Hero, About, Technologies, Experience, Projects
+    Navbar, Hero, About, Technologies, Experience, Projects, Contact
 };
