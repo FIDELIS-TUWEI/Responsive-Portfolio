723f5799788cffb287d298275c21bf1b53f50958
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-27 12:08:19 +0300

    Projects component dynamic import

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,8 @@ import Hero from "./Hero";
 import About from "./About";
 import Technologies from "./Technologies";
 import Experience from "./Experience";
+import Projects from "./Projects";
 
 export {
-    Navbar, Hero, About, Technologies, Experience
+    Navbar, Hero, About, Technologies, Experience, Projects
 };
