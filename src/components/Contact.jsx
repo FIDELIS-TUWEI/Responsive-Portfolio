28e4848d8204009a1911488dbce6241b2cbec047
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-27 13:36:34 +0300

    Contact UI design and styles complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,15 @@
+import { CONTACT } from "../constants";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="border-b border-neutral-900 pb-20">
+        <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+        <div className="text-center tracking-tighter">
+            <p className="my-4">{CONTACT.address}</p>
+            <p className="my-4">{CONTACT.phoneNo}</p>
+            <a href="#home" className="border-b">{CONTACT.email}</a>
+        </div>
+    </div>
   )
 }
 
