ab7c5f56927a244733b6254f35826cc09155ee1b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-27 11:44:51 +0300

    Experience component dynamic import

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,8 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
 import Technologies from "./Technologies";
+import Experience from "./Experience";
 
 export {
-    Navbar, Hero, About, Technologies
+    Navbar, Hero, About, Technologies, Experience
 };
